beceff61b35bbed5a14e16ddae9d8f9914404ef8
kspace-trk <keigonakao.0825@gmail.com>
2025-08-29 22:20:59 +0900

    feat: indexページの見た目作成

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,59 @@
 <script setup lang="ts">
+import { MainButton, TextareaField } from 'admin-ui-components';
+
+const text = ref('');
+const result = ref('');
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div id="index">
+    <div class="container">
+      <TextareaField
+        v-model="text"
+        class="textarea-input"
+        label="どんな要件？"
+      />
+      <div class="button-container">
+        <MainButton text="生成する" />
+      </div>
+
+      <TextareaField
+        v-model="result"
+        class="textarea-result"
+        label="生成結果"
+        rows="10"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+#index {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  max-width: 680px;
+  width: 100%;
+  padding: 0 16px;
+}
+
+.textarea-input {
+  margin-top: 80px;
+}
+
+.button-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 28px;
+}
+
+.textarea-result {
+  margin-top: 120px;
+}
 </style>
