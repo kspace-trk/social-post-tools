4a4f254e880ae054a58081a7025b5250316b2524
kspace-trk <keigonakao.0825@gmail.com>
2025-09-19 17:19:34 +0900

    fix: nuxt configでcssは読み込まない

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,10 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  css: [
-    '@kspace-trk/admin-ui-components/runtime/assets/css/reset.css',
-    '@kspace-trk/admin-ui-components/runtime/assets/css/fonts/mplus-1p.css',
-  ],
+  // css: [
+  //   '@kspace-trk/admin-ui-components/runtime/assets/css/reset.css',
+  //   '@kspace-trk/admin-ui-components/runtime/assets/css/fonts/mplus-1p.css',
+  // ],
 
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
