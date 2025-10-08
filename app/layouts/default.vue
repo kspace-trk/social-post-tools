c5512a81269275c93f9e01a859e604fa82c3a8a3
kspace-trk <keigonakao.0825@gmail.com>
2025-10-08 22:00:29 +0900

    feat: KSDashboardContainerを使用するようにする

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -48,45 +48,29 @@ const getPageTitle = (): string => {
 };
 
 const pageTitle = computed(() => getPageTitle());
+
+const sideHeaderProps = computed(() => ({
+  logoText: 'social-post-tools',
+  menuItems: menuItems,
+  currentPath: route.path,
+  bottomMenuItem: bottomMenuItem,
+}));
+
+const topHeaderProps = computed(() => ({
+  title: pageTitle.value,
+}));
 </script>
 
 <template>
   <div id="default-layout">
-    <KSSideHeader
-      logo-text="social-post-tools"
-      :menu-items="menuItems"
-      :current-path="route.path"
-      :bottom-menu-item="bottomMenuItem"
-    />
-    <div class="main-content">
-      <KSTopHeader :title="pageTitle" />
+    <KSDashboardContainer
+      :side-header-props="sideHeaderProps"
+      :top-header-props="topHeaderProps"
+    >
       <slot />
-    </div>
+    </KSDashboardContainer>
   </div>
 </template>
 
 <style lang="scss" scoped>
-#default-layout {
-  display: flex;
-
-  .main-content {
-    margin-left: 260px; // SideHeaderの幅分だけ右にずらす
-    padding-top: 73px; // TopHeaderの高さ分だけ下にずらす
-    width: calc(100% - 260px); // SideHeaderの幅を除いた幅
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh; // 最小の高さを画面の高さに設定
-    transition: margin-left 0.3s ease, width 0.3s ease; // スムーズなアニメーション
-  }
-}
-
-// モバイル対応：768px以下の場合
-@media (max-width: 768px) {
-  #default-layout {
-    .main-content {
-      margin-left: 0; // SideHeaderが隠れている時は左マージンなし
-      width: 100%; // 全幅で表示
-    }
-  }
-}
 </style>
